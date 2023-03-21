76cf18df5839d07672c236d081a1a302e7219fa2
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-03-21 19:55:31 +0300

    15. Mouse World Position

--- Assets/_Scripts/MouseWorld.cs
+++ Assets/_Scripts/MouseWorld.cs
@@ -5,10 +5,31 @@ namespace _Scripts
 {
     public class MouseWorld : MonoBehaviour
     {
+        private static MouseWorld Instance;
+        
+        
+        [SerializeField] private LayerMask mousePlaneLayerMask;
+
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+
         private void Update()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.Log(Physics.Raycast(ray));
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            Debug.Log(Physics.Raycast(ray,out RaycastHit raycastHit, float.MaxValue, mousePlaneLayerMask));
+            transform.position = raycastHit.point;
+        }
+        
+        
+        public static Vector3 GetPosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            Physics.Raycast(ray,out RaycastHit raycastHit, float.MaxValue, Instance.mousePlaneLayerMask);
+            return raycastHit.point;
         }
     }
 }
