4e23192ce24181719de6b772113a0c262beb6571
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-03-21 15:29:43 +0300

    13. Unit Move

--- Assets/_Scripts/Unit.cs
+++ Assets/_Scripts/Unit.cs
@@ -1,8 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+namespace _Scripts.test
 {
-    
+    public class Unit : MonoBehaviour
+    {
+        private Vector3 targetPosition;
+
+
+        private void Update()
+        {
+            float stoppingDistance = .1f;
+            if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+            {
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+                float moveSpeed = 4f;
+                transform.position += moveDirection * (Time.deltaTime * moveSpeed);
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Move(new Vector3(4, 0, 4));
+            }
+        }
+
+
+        private void Move(Vector3 targetPosition)
+        {
+            this.targetPosition = targetPosition;
+        }
+    }
 }
